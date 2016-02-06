6a9808ad5ee613dc4118af88d6db43a8ac92d93a
Ryan Guo <ryan@islandzero.net>
2016-02-06 23:47:56 +0800

    init

---
+++ Deliria/Deliria.h
@@ -0,0 +1,19 @@
+//
+//  Deliria.h
+//  Deliria
+//
+//  Created by Yanke Guo on 16/2/6.
+//  Copyright © 2016年 JuXian(Beijing) Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Deliria.
+FOUNDATION_EXPORT double DeliriaVersionNumber;
+
+//! Project version string for Deliria.
+FOUNDATION_EXPORT const unsigned char DeliriaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Deliria/PublicHeader.h>
+
+
