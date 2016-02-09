6a778a150d5c9bd7588f4d47a54f6b82e3b97753
Ryan Guo <ryan@islandzero.net>
2016-02-09 17:06:14 +0800

    add UIKit support

---
+++ Deliria/UIKit/UIGeometry/CGRect+UIExtension.swift
@@ -0,0 +1,34 @@
+//
+//  CGRect+UIExtension.swift
+//  Deliria
+//
+//  Created by Yanke Guo on 16/2/9.
+//  Copyright © 2016年 JuXian(Beijing) Technology Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+/**
+ Create a new CGRect from CGRect and UIEdgeInsets
+
+ - parameter lhs: CGRect
+ - parameter rhs: UIEdgeInsets
+
+ - returns: new CGRect
+ */
+public func +(lhs: CGRect, rhs: UIEdgeInsets) -> CGRect {
+  return UIEdgeInsetsInsetRect(lhs, rhs)
+}
+
+/**
+ Apply a UIEdgeInsets to CGRect
+
+ - parameter lhs: CGRect
+ - parameter rhs: UIEdgeInsets
+
+ - returns: lhs
+ */
+public func +=(inout lhs: CGRect, rhs: UIEdgeInsets) -> CGRect {
+  lhs = UIEdgeInsetsInsetRect(lhs, rhs)
+  return lhs
+}
---
+++ Deliria/UIKit/UIGeometry/UIOffset+UIExtension.swift
@@ -0,0 +1,50 @@
+//
+//  UIOffset+UIExtension.swift
+//  Deliria
+//
+//  Created by Yanke Guo on 16/2/9.
+//  Copyright © 2016年 JuXian(Beijing) Technology Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+extension UIOffset: Vector2DConvertible, ArrayLiteralConvertible {
+
+  public var x: CGFloat {
+    get {
+      return self.horizontal
+    }
+    set(value) {
+      self.horizontal = value
+    }
+  }
+
+  public var y: CGFloat {
+    get {
+      return self.vertical
+    }
+    set(value) {
+      self.vertical = value
+    }
+  }
+
+  public init(x: CGFloat, y: CGFloat) {
+    self.horizontal = x
+    self.vertical   = y
+  }
+
+  public typealias Element = CGFloat
+
+  public init(arrayLiteral elements: CGPoint.Element...) {
+    self.init(horizontal: elements[0], vertical: elements[1])
+  }
+
+}
+
+extension Vector2DConvertible {
+
+  public var offset: UIOffset {
+    return UIOffset(horizontal: self.x, vertical: self.y)
+  }
+
+}
